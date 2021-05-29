9d6aac47d62e744aad19dc39642b642e07467e7c
Aryan nath <aryannath2@gmail.com>
2021-05-29 14:31:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,14 +14,14 @@ def get_city(city):
     api_key = config.weather_api_key
     base_url = "https://api.openweathermap.org/data/2.5/forecast?"            
 
-    complete_url = base_url + "q=" + city + "&appid=" + api_key        #city
+    complete_url = base_url + "q=" + city + "&appid=" + api_key        
     response = requests.get(complete_url)
     x = response.json()
 
     if x["cod"] != "404":
         y = x["list"]
-        t1 =  ((y[7].main.temp + y[14].main.temp + y[21].main.temp + y[28].main.temp+y[35].main.temp)/5)
-        h1 =  ((y[7].main.humidity+ y[14].main.humidity + y[21].main.humidity + y[28].main.humidity+y[35].main.humidity)/5)
+        t1 = ((y[7]['main']['temp'] + y[14]['main']['temp'] + y[21]['main']['temp'] + y[28]['main']['temp']+y[35]['main']['temp'])/5)
+        h1 = ((y[7]['main']['humidity']+ y[14]['main']['humidity'] + y[21]['main']['humidity'] + y[28]['main']['humidity']+y[35]['main']['humidity'])/5)
 
         temp = round (( t1 - 273.15), 2)
         humi = h1
@@ -36,7 +36,7 @@ def get_city(city):
 
 app = Flask(__name__)
 
-
+cityy=None
 
 # render home page
 
@@ -44,31 +44,33 @@ app = Flask(__name__)
 @ app.route('/')
 def home():
     title = 'SoilUp Home page'
-    return render_template('index.html', title=title) #this title can be used in the front end from here the backend
+    return render_template('index.html') #this title can be used in the front end from here the backend
 
 # render crop recommendation form page
 
 
 @ app.route('/city', methods=['GET','POST']) #maybe a herf should be used 
 def city():
-        #if request.method == "GET": 
-         cityy = request.args.get("city")   #state = request.form.get("stt") ,,this is not used because we don't need home state
+       # if request.method == "GET": 
+       #  cityy = request.args.get("city")   #state = request.form.get("stt") ,,this is not used because we don't need home state
         # return render_template('test.html' , ci=cityy) 
-         return render_template('city.html' ,ci=cityy)  
+         return render_template('city.html')     # cityy ke andr data hi nhi ara hai shyd this is due to request method post
 
-@ app.route('/crop-recommend', methods=['POST']) #this slash is like the slash we have in an web URL
+@ app.route('/crop-recommend', methods=['GET','POST']) #this slash is like the slash we have in an web URL
 def crop_recommend():
-    title = 'SoilUp - Crop Rec'
-    return render_template('lab.html', title=title)
+    
+    global cityy
+    cityy = request.args.get("city")
+    return render_template('lab.html')
 
 
 @ app.route('/crop-predict', methods=['POST'])
 def crop_prediction():
-    title = 'SoilUp - Crop Pred'
+   
     
    
    
-    if request.method == 'POST':
+    if request.method == 'POST':                #we can also make a new block like nitrogen for city
         N = int(request.form['nitrogen'])
         P = int(request.form['phosphorous'])
         K = int(request.form['pottasium'])
@@ -76,7 +78,9 @@ def crop_prediction():
         rainfall = float(request.form['rainfall'])
         
 
+       
         
+
 
         if get_city(cityy) != None:
             temp, humi = get_city(cityy)
@@ -84,11 +88,11 @@ def crop_prediction():
             my_prediction = model.predict(data)
             final_prediction = my_prediction[0]
 
-            return render_template('result.html', prediction=final_prediction, title=title)
+            return render_template('result.html', prediction=final_prediction,tt=temp,hh=humi)
 
         else:
 
-            return render_template('try_again.html', title=title)
+            return render_template('try_again.html')
 
 
 if __name__ == '__main__':
